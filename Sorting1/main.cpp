9577f5c1d39cbd20862579e143bfe0688404b373
David Thacker <davidt@alooma.com>
2013-04-11 01:08:19 -0500

    Initial commit

---
+++ Sorting1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Sorting1
+//
+//  Created by David Thacker on 4/11/13.
+//  Copyright (c) 2013 David Argyle Thacker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
